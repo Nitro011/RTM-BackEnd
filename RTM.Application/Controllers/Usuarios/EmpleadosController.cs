192fa5b3f530b02411bc056b5a63f436ce97be4c
ArquimedesFermin <arquimedesfermin17@gmail.com>
2020-06-02 20:33:23 -0400

    '.'

---
+++ RTM.Application/Controllers/Usuarios/EmpleadosController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RTM.Models;
+using RTM.Repository.Interface;
+
+namespace RTM.Application.Controllers.Usuarios
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpleadosController : ControllerBase
+    {
+
+
+
+        private readonly IUnitOfWork _UnitOfWork;
+        private readonly IGenericRepository<Empleado> _GenericRepository;
+
+        public EmpleadosController(IUnitOfWork UnitOfWork, IGenericRepository<Empleado> GenericRepository)
+        {
+            this._UnitOfWork = UnitOfWork;
+            this._GenericRepository = GenericRepository;
+        }
+
+
+        // GET: api/Usuarios
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> lista()
+        {
+            try
+            {
+
+                var GetUser = await _UnitOfWork.context.Empleados.ToListAsync();
+
+                return Ok(new Request()
+                {
+                    status = true,
+                    message = "Esta accion se ejecuto correctamente",
+                    data = GetUser
+                });
+            }
+            catch (Exception ex)
+            {
+                return Ok(new Request()
+                {
+                    status = true,
+                    message = "El usuario se registro correctamente",
+                    data = ex.Message
+                });
+            }
+
+
+
+        }
+
+        // GET: api/Usuarios/5
+        [HttpGet]
+        [Route("[action]/{id}")]
+        public async Task<IActionResult> listaPorId(int id)
+        {
+            try
+            {
+
+                var GetUser = await _UnitOfWork.context.Empleados.Where(x => x.EmpleadoID == id).FirstOrDefaultAsync();
+
+                return Ok(new Request()
+                {
+                    status = true,
+                    message = "Esta accion se ejecuto correctamente",
+                    data = GetUser
+                });
+            }
+            catch (Exception ex)
+            {
+                return Ok(new Request()
+                {
+                    status = true,
+                    message = "El usuario se registro correctamente",
+                    data = ex.Message
+                });
+            }
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> register([FromBody] Empleado empleado)
+        {
+            try
+            {
+             
+              
+                await _GenericRepository.Add(empleado);
+                _UnitOfWork.Commit();
+
+
+                return Ok(new Request()
+                {
+                    status = true,
+                    message = "El usuario se registro correctamente",
+                    data = empleado
+                });
+            }
+            catch (Exception ex)
+            {
+
+                return Ok(new Request()
+                {
+                    status = true,
+                    message = "El usuario se registro correctamente",
+                    data = ex.Message
+                });
+            }
+
+
+        }
+
+        // PUT: api/Usuarios/5
+        [HttpPut]
+        [Route("[action]")]
+        public async Task<IActionResult> modificar([FromBody] Empleado empleado)
+        {
+            try
+            {
+
+                await _GenericRepository.Update(empleado); 
+                 _UnitOfWork.Commit();
+
+                return Ok(new Request()
+                {
+                    status = true,
+                    message = "Esta accion se ejecuto correctamente",
+                    data = empleado
+                    
+                });
+            }
+            catch (Exception ex)
+            {
+                return Ok(new Request()
+                {
+                    status = true,
+                    message = "El usuario se registro correctamente",
+                    data = ex.Message
+                });
+            }
+
+        }
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- RTM.Persistence/Migrations/20200602235219_UpdateClientes.Designer.cs
+++ RTM.Persistence/Migrations/20200602235219_UpdateClientes.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RTM.Persistence;
 
 namespace RTM.Persistence.Migrations
 {
     [DbContext(typeof(RTMDbContext))]
-    partial class RTMDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200602235219_UpdateClientes")]
+    partial class UpdateClientes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
