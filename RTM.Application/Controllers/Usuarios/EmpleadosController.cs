ce537a75c6f6d6c3175b492ccbf71319c403c753
ArquimedesFermin <arquimedesfermin17@gmail.com>
2020-06-03 02:14:46 -0400

    '.'

--- RTM.Application/Controllers/Usuarios/EmpleadosController.cs
+++ RTM.Application/Controllers/Usuarios/EmpleadosController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RTM.Models;
+using RTM.Models.DTO;
 using RTM.Repository.Interface;
 
 namespace RTM.Application.Controllers.Usuarios
@@ -88,13 +89,44 @@ namespace RTM.Application.Controllers.Usuarios
 
         [HttpPost]
         [Route("[action]")]
-        public async Task<IActionResult> register([FromBody] Empleado empleado)
+        public async Task<IActionResult> register([FromBody] UsuariosEmpleadosDTO usuario)
         {
             try
             {
-             
-              
-                await _GenericRepository.Add(empleado);
+
+
+
+                var empleado = new Empleado()
+                {
+                    Nombres = usuario.Nombres,
+                    Apellidos = usuario.Apellidos,
+                    Sexo = usuario.Sexo,
+                    Cedula = usuario.Cedula,
+                    Fecha_Nacimiento = usuario.Fecha_Nacimiento,
+                    Edad = usuario.Edad,
+                    Direccion = usuario.Direccion,
+                    Telefono = usuario.Telefono
+
+                };
+
+                await _UnitOfWork.context.Empleados.AddAsync(empleado);
+                _UnitOfWork.Commit();
+
+
+                var user = new Usuario()
+                {
+
+                    UserName = usuario.UserName,
+                    LockoutEnabled = true,
+                    Email = usuario.Email,
+                    RolID = usuario.RolID,
+                    EmpleadoID = empleado.EmpleadoID
+
+                };
+
+                var pass = Encriptar(usuario.PasswordHash);
+                user.PasswordHash = pass;
+                await _UnitOfWork.context.Usuarios.AddAsync(user);
                 _UnitOfWork.Commit();
 
 
@@ -153,6 +185,14 @@ namespace RTM.Application.Controllers.Usuarios
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+        }
+
+        private string Encriptar(string _cadenaAencriptar)
+        {
+            string result = string.Empty;
+            byte[] encryted = System.Text.Encoding.Unicode.GetBytes(_cadenaAencriptar);
+            result = Convert.ToBase64String(encryted);
+            return result;
         }
     }
 }
