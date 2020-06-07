6691dd5c0afeafe9b2b438c24bc27d31c740ad71
ArquimedesFermin <arquimedesfermin17@gmail.com>
2020-06-07 02:26:54 -0400

    '.'

---
+++ RTM.Models/DTO/EmpleadosListView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTM.Models.DTO
+{
+   public class EmpleadosListView
+    {
+
+        public int Id { get; set; }
+        public string NombreCompleto { get; set; }
+        public string Puesto { get; set; }
+    }
+}
