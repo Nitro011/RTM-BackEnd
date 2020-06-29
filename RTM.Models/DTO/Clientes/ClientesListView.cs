e57a99f6ab4f4baaa54ffd8b0e2d89d8bf36e619
ANDRES EUGENIO GUERRERO CORDERO <andresgc1997@outlook.com>
2020-06-29 01:28:07 -0400

    backEndV4

---
+++ RTM.Models/DTO/Clientes/ClientesListView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTM.Models.DTO.Clientes
+{
+    public class ClientesListView
+    {
+        public int ClienteID { get; set; }
+        public string Nombre_Cliente { get; set; }
+        public string Correo_Electronico { get; set; }
+        public string No_Telefono { get; set; }
+        public string Direccion { get; set; }
+    }
+}
