f872b9d3aadabc3de5f0e419aef06f082e4a524a
ArquimedesFermin <arquimedesfermin17@gmail.com>
2020-07-10 00:02:12 -0400

    .

--- RTM.Models/DTO/Orden_Cliente/OrdenesClientesDetalles.cs
+++ RTM.Models/DTO/Orden_Cliente/OrdenesClientesDetalles.cs
@@ -14,7 +14,7 @@ namespace RTM.Models.DTO.Orden_Cliente
         public string Marcar { get; set; }
 
         public  List<DetallesColores> Ordenes_Clientes_Detalles_Colores { get; set; }
-        public  List<DetallesDimension> Ordenes_Clientes_Detalles_Dimensiones { get; set; }
+        public  List<DetallesDimension> Ordenes_Clientes_Detalles_Size { get; set; }
         public  List<DetallesModelos> Ordenes_Clientes_Detalles_Modelos { get; set; }
         public  List<DetallesCalzado> Ordenes_Clientes_Detalles_Tipos_Calzados { get; set; }
     }
